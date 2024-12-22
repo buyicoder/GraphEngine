8ac06e7d978e810c76410a27cb9cf725e2bb86bf
nanmu <zhanyongjie4@gmail.com>
2024-12-22 21:27:40 +0800

    加入网格和提示坐标轴，但是不知道为什么用不了着色器上不了色，且main.cpp变得非常臃肿，为了推进度将就一下先吧

--- src/utils/ModelSystem.cpp
+++ src/utils/ModelSystem.cpp
@@ -3,11 +3,13 @@
 // 变量定义
 std::vector<int> vertexCountIndices;
 std::vector<std::unique_ptr<Model>> meshes;
+vecf3 pos = {0.0f,0.0f,0.0f};
 int current_index = 0;
 float simplification_ratio = 0.99f;
 bool shows_border = false;
 bool isInsideVertexCountsWindow = false;
 bool isButtonClicked = false;
+bool isRotating = true;
 
 // Getter 函数定义
 std::vector<int>* getVertexCountIndices() {
@@ -43,4 +45,29 @@ bool* getIsButtonClicked() {
 void load_meshes() {
 	meshes.emplace_back(Model::load(RESOURCES_DIR"/squirrel.obj"));
 	vertexCountIndices.push_back(0);
+}
+
+void DrawModel(Shader shader,Shader border_shader) {
+    // render the model
+    vecf3 model_pos(pos);
+    static float angle_y;
+    if(isRotating)angle_y= base_angle_y + static_cast<float>(glfwGetTime());
+    // rot matrix
+    matf4 model_transform;
+    model_transform << cos(angle_y), 0.0f, sin(angle_y), model_pos[0],
+        0.0f, 1.0f, 0.0f, model_pos[1],
+        -sin(angle_y), 0.0f, cos(angle_y), model_pos[2],
+        0.0f, 0.0f, 0.0f, 1.0f;
+    shader.set_matf4("model", model_transform);
+    meshes[current_index]->va->draw(shader);
+
+    //render borders
+    if (shows_border) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        border_shader.set_vecf3("camera_pos", camera.position);
+        border_shader.set_matf4("projection", camera.get_projection_matrix(SCR_WIDTH, SCR_HEIGHT, 0.1f, 100.0f));
+        border_shader.set_matf4("view", camera.get_view_matrix());
+        border_shader.set_matf4("model", model_transform);
+        meshes[current_index]->va->draw(border_shader);
+    }
 }
